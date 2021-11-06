89a53acd034f7aa16d1fe937752e626bc0ea72ee
jiko21 <daikikojima0531@icloud.com>
2021-11-06 16:09:43 +0900

    add: storybook

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "webpackFinal": async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': path.join(process.cwd(), 'node_modules/@emotion/react'),
+          '@emotion/styled': path.join(process.cwd(), 'node_modules/@emotion/styled'),
+          'emotion-theming': path.join(process.cwd(), 'node_modules/@emotion/react'),
+        }
+      }
+    }
+  }
+}
