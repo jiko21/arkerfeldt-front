f198a7166dafb0acb8b99e3c06a494c4d3c7dbe5
jiko21 <daikikojima0531@icloud.com>
2021-11-29 22:47:18 +0900

    add: service for auth

---
+++ test/storyshots.ts
@@ -0,0 +1,14 @@
+// import initStoryshots from '@storybook/addon-storyshots';
+// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+// import path from 'path';
+//
+// initStoryshots({
+//   integrityOptions: { cwd: __dirname },
+//   test: imageSnapshot({
+//     storybookUrl: `file://${path.resolve(__dirname, '../storybook-static')}`,
+//     getMatchOptions: () => ({
+//       failureThreshold: 0.1,
+//       failureThresholdType: 'percent',
+//     }),
+//   }),
+// });
