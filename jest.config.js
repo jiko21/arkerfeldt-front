0da54fa859d867625ec914a573a60ce56c509434
jiko21 <daikikojima0531@icloud.com>
2022-01-23 23:03:31 +0900

    use: @swc/jest

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,6 @@ module.exports = {
   snapshotSerializers: [
     '@emotion/jest/serializer' /* if needed other snapshotSerializers should go here */,
   ],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
-  },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
   },
@@ -18,8 +15,30 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
   globals: {
-    'ts-jest': {
+    '@swc/jest': {
       tsconfig: '<rootDir>/test/tsconfig.jest.json',
     },
+  },
+  transform: {
+    '.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        sourceMaps: true,
+        module: {
+          type: 'commonjs',
+        },
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
   },
 };
